37e185ad86e18872dc8fa545c892618b769245dc
wildoranges <zkd18cjb@mail.ustc.edu.cn>
2022-11-20 20:52:03 +0800

    update test scipt

--- lab1/test.sh
+++ lab1/test.sh
@@ -1,4 +1,14 @@
+#!/bin/bash
 ./dlc bits.c
 make clean
 make btest
-./btest -f $1
+if [ $? -ne 0 ]; then
+    echo "make failed!"
+    exit 1
+fi
+
+if [ $# -gt 0 ]; then
+    ./btest -f $1
+else
+    ./btest
+fi
