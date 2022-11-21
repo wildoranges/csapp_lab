2ecea96414727a4e231071bdd876b2ffe14b8cd4
wildoranges <zkd18cjb@mail.ustc.edu.cn>
2022-11-21 17:35:06 +0800

    update sol for lab3

--- lab3/put_string_q1.c
+++ lab3/put_string_q1.c
@@ -15,13 +15,15 @@ int out2[] = {0x48, 0x48, 0x48, 0x48,
               0x20, 0x8e, 0x04, 0x08, 0x03, 0x00};
 
 int main() {
-    //printf("%s", out);
-    for(int i = 0; i < strlen(out); i++) {
+    for (int i = 0; i < strlen(out) - 1; i++) {
         if (out2[i] < 16) {
             printf("0");
         }
         printf("%x ", out2[i]);
     }
-    printf("0%x", 0);
+    if (out2[strlen(out) - 1] < 16) {
+            printf("0");
+    }
+    printf("%x", out2[strlen(out) - 1]);
     return 0;
 }
